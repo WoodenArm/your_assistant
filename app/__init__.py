59a08dac058c1cbc63e5eff2e2b416fd4a966f25
andriy <andriygoroshko@gmail.com>
2023-06-18 10:39:08 +0300

    flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 from config import Config
 from app.extensions import db
+from flask_migrate import Migrate
+from app.models import user
+
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -9,6 +13,7 @@ def create_app(config_class=Config):
 
     # Initialize Flask extensions here
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Register blueprints here
     from app.main import bp as main_bp
